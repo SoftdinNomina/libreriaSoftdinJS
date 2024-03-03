3473d735ab4c31bd9427f696c82fb2335c461823
deivibarra <deivibarra@yahoo.com>
2024-03-02 20:58:03 -0500

    Agregando funcion EnumClaseEmpresa

---
+++ EnumClaseEmpresa.js
@@ -0,0 +1,29 @@
+class EnumClaseEmpresa {
+    static EMPRESA = 1;
+    static COOPERATIVA = 2;
+    static BANCO = 3;
+    static PILA = 4;
+
+    static descriptions = [
+        { id: EnumClaseEmpresa.EMPRESA, code: 'EMP', description: 'EMPRESA' },
+        { id: EnumClaseEmpresa.COOPERATIVA, code: 'COP', description: 'COOPERATIVA' },
+        { id: EnumClaseEmpresa.BANCO, code: 'BAN', description: 'BANCO' },
+        { id: EnumClaseEmpresa.PILA, code: 'PIL', description: 'PILA' },
+    ];
+
+    static getCollection() {
+        return EnumClaseEmpresa.descriptions;
+    }
+
+    static getById(id) {
+        return EnumClaseEmpresa.descriptions.find(item => item.id === id) || null;
+    }
+
+    static getAll() {
+        return EnumClaseEmpresa.descriptions;
+    }
+
+    static getByDescription(description) {
+        return EnumClaseEmpresa.descriptions.find(item => item.description === description) || null;
+    }
+}
