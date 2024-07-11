23d0cd7ce47316a5cc675927d651578ef3b33272
deivibarra <deivibarra@yahoo.com>
2024-07-11 07:33:50 -0500

    Ajustando EnumRespuestaSINO

--- src/EnumRespuestaSINO.js
+++ src/EnumRespuestaSINO.js
@@ -18,6 +18,32 @@ class EnumRespuestaSINO {
     static getByDescription(description) {
         return EnumRespuestaSINO.descriptions.find(item => item.description === description) || null;
     }
+
+    static colorMapping = [
+        { 'success': EnumRespuestaSINO.SI },
+        { 'danger': EnumRespuestaSINO.NO },
+    ];
+
+    static getColors(campo) {
+        const colorArray = {};
+
+        for (const color of EnumRespuestaSINO.colorMapping) {
+            const description = Object.values(color)[0];
+            const descriptionEntry = EnumRespuestaSINO.descriptions.find(item => item.id === description);
+            if (descriptionEntry) {
+                colorArray[Object.keys(color)[0]] = descriptionEntry[campo];
+            } else {
+                colorArray[Object.keys(color)[0]] = null; // Handle the case where the field does not exist
+            }
+        }
+        return colorArray;
+    }
+
+    static getColorName(campo, valor) {
+        const colors = EnumRespuestaSINO.getColors(campo);
+        return Object.keys(colors).find(key => colors[key] === valor);
+    }
+
 }
 
 module.exports = EnumRespuestaSINO;
