3bfdb33c6b5b70120a3dd6d9be6e82c27317f3fb
deivibarra <deivibarra@yahoo.com>
2024-11-16 18:45:30 -0500

    Ajustando el EnumModulos

--- src/EnumModulos.js
+++ src/EnumModulos.js
@@ -1,6 +1,10 @@
 class EnumModulos {
   static CONTRATACION = 1;
-  static PRESTAMO = 2;
+  static LIQ_NOMINA = 2;
+  static LIQ_VACACIONES = 3;
+  static LIQ_PRESTSOCIALES = 4;
+  static LIQ_CONTRATO = 5;
+  static LIQ_PRESTAMO = 6;
   // static SaldosIniciales = 14;
 
   static descriptions = [
@@ -9,7 +13,31 @@ class EnumModulos {
       code: "CONTRATACION",
       description: "Contratación",
     },
-    { id: EnumModulos.PRESTAMO, code: "PRESTAMO", description: "Prestamo" },
+    {
+      id: EnumModulos.LIQ_NOMINA,
+      code: "LIQ_NOMINA",
+      description: "Liquidación de Nómina",
+    },
+    {
+      id: EnumModulos.LIQ_VACACIONES,
+      code: "LIQ_VACACIONES",
+      description: "Liquidación de Vacaciones",
+    },
+    {
+      id: EnumModulos.LIQ_PRESTSOCIALES,
+      code: "LIQ_PRESTSOCIALES",
+      description: "Liquidación de Prestaciones Sociales",
+    },
+    {
+      id: EnumModulos.LIQ_CONTRATO,
+      code: "LIQ_CONTRATO",
+      description: "Liquidación de Contrato",
+    },
+    {
+      id: EnumModulos.LIQ_PRESTAMO,
+      code: "PRESTAMO",
+      description: "Préstamo",
+    }
   ];
 
   static getById(id) {
